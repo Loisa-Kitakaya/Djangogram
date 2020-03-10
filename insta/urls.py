bf375889ca2366e86ab4b66e7da2cd07f31e2560
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-10 12:55:00 +0300

    created urls and view functions for the applications authentication

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # URLconf
 
 urlpatterns = [
     path("", views.home_page, name="home_page"),
+    path("dashboard", views.dashboard),
+    path("logout", views.logout),
+    path("", include("django.contrib.auth.urls")),
+    path("", include("social_django.urls")),
 ]
