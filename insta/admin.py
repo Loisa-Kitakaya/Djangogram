dea78c9dbfefab57d9028506fc0eef4c287a9339
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-10 10:58:33 +0300

    reregistered the models on admin page

--- insta/admin.py
+++ insta/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(ImageModel)
-admin.site.register(ProfileModel)
+admin.site.register(Image)
+admin.site.register(Profile)
