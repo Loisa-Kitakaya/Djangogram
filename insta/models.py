d8dcf5f9f2f8f0ff52c090f797261e51ef942c8c
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-03-10 10:59:19 +0300

    created new models

--- insta/models.py
+++ insta/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 
-class ImageModel(models.Model):
+class Profile(models.Model):
+    profile_dp = models.ImageField(upload_to="media/")
+    profile_name = models.CharField(max_length=30)
+    profile_password = models.CharField(max_length=20)
+    profile_biography = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.profile_username
+
+
+class Image(models.Model):
     image = models.ImageField(upload_to="media/")
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=200)
     image_comments = models.CharField(max_length=300)
-    profile = models.ForeignKey(ProfileModel, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.image_caption
-
-
-class ProfileModel(models.Model):
-    profile_pic = models.ImageField(upload_to="media/")
-    profile_username = models.CharField(max_length=30)
-    profile_userpassword = models.CharField(max_length=20)
-    profile_bio = models.CharField(max_length=400)
-
-    def __str__(self):
-        return self.profile_username
 
 
 """
@@ -30,17 +30,17 @@ image model methods
 
 ## save_image
 @classmethod
-def save_image(cls=ImageModel):
+def save_image(cls=Image):
     pass
 
 
 ## delete_method
 @classmethod
-def delete_image(cls=ImageModel):
+def delete_image(cls=Image):
     pass
 
 
 ## update_caption
 @classmethod
-def update_caption(cls=ImageModel):
+def update_caption(cls=Image):
     pass
